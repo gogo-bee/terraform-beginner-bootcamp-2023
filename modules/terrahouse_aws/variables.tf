fa982d6bb06b5d81e9e2d88a63ad25b1ab5f7ad4
Gogo <ogbobeniann@gmail.com>
2023-11-04 16:43:01 +0000

    #29 Enabled static website hosting, uploaded index and error file via terraform

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -17,3 +17,23 @@ variable "bucket_name" {
     error_message = "Bucket name must be between 3 and 63 characters long"
   }
 } 
+
+variable "index_html_filepath" {
+  description = "Filepath for the index.html file"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exist"
+  }
+}
+
+variable "error_html_filepath" {
+  description = "Filepath for the error.html file"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The provided path for error.html does not exist"
+  }
+}
