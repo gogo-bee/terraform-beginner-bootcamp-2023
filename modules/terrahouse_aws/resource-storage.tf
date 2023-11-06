b539c32869796891eabd96b021046d269f3d8d00
Gogo <146005920+gogo-bee@users.noreply.github.com>
2023-11-06 20:10:34 +0100

    #42 fix code trap (#43)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -37,12 +37,17 @@ resource "aws_s3_object" "index_html" {
   }
 }
 
+locals {
+  asset_files = fileset(var.assets_path, "*.{jpg,png,gif}")
+}
+
+
 resource "aws_s3_object" "upload_assets" {
-  for_each = fileset(var_assets_path, "*.{jpg,png,gif}")
+  for_each = fileset(var.assets_path, "*.{jpg,png,gif}")
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "assets/${each.key}"
-  source = "${var_assets_path}/${each.key}"
-  etag = filemd5("${var_assets_path}/${each.key}")
+  source = "${var.assets_path}${each.key}"
+  etag   = filemd5("${var.assets_path}/${each.key}")
   lifecycle {
     replace_triggered_by = [terraform_data.content_version.output]
     ignore_changes = [etag]
